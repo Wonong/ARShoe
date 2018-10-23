c5cdb3c1e07e6f6d37428566aaf4b4728a497d5e
snowflake-1117 <1117hyemin@gmail.com>
2018-10-23 16:12:34 +0900

    Add a Setting file for shoe scaling.

---
+++ Assets/Scripts/CNN/Setting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Setting : MonoBehaviour
+{
+    public GameObject settingPanel;
+    public InputField inputField;
+    public DetectorController detector;
+    public Button confirmButton;
+
+    private void Start()
+    {
+        detector = FindObjectOfType<DetectorController>();
+    }
+
+    public void ClickSetting()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void ClickConfirmButton()
+    {
+        Debug.Log("Click confirm");
+        detector.shoeScale = float.Parse(inputField.text);
+        detector.SetShoeScale();
+    }
+}

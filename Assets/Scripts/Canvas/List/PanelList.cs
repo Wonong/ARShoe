7bf7f6d636d21987f9fc9bb47437d249c7d54602
Wonyeong Choi <ciwnyg0815@gmail.com>
2018-10-30 16:29:55 +0900

    add list scroll function

--- Assets/Scripts/Canvas/List/PanelList.cs
+++ Assets/Scripts/Canvas/List/PanelList.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class PanelList : MonoBehaviour {
 
+    public Camera cam;
+    public Vector2 minPos, maxPos;
+
 	// Use this for initialization
 	void Start () {
-        //GetComponent<ScrollRect>().OnScroll(OnScrollChanged);
+        GetComponent<ScrollRect>().onValueChanged.AddListener(OnScrollChanged);
 	}
 	
 	// Update is called once per frame
@@ -15,7 +18,8 @@ public class PanelList : MonoBehaviour {
 		
 	}
 
-    void OnScrollChanged(){
-        Debug.Log("");
+    void OnScrollChanged(Vector2 scrollPos){
+        //Debug.Log(scrollPos.ToString("F4"));
+        cam.transform.position = Vector2.Lerp(minPos, maxPos, scrollPos.y);
     }
 }

932cf6b8b093d8a7f9903f410b183956d9afae3e
Wonyeong Choi <ciwnyg0815@gmail.com>
2018-11-01 16:11:16 +0900

    fix DB error

--- Assets/Scripts/Canvas/List/PanelList.cs
+++ Assets/Scripts/Canvas/List/PanelList.cs
@@ -8,12 +8,14 @@ public class PanelList : ViewController {
     private NavigationViewController navigationView;
     public Camera cam;
     public Vector2 minPos, maxPos;
+    public ShoeRow newRow, bestRow;
 
 	// Use this for initialization
 	void Start () {
 
         navigationView = UIManager.Instance.navigationView;
         GetComponent<ScrollRect>().onValueChanged.AddListener(OnScrollChanged);
+        AddShoeItemsIntoRow();
 
         // navigation view의 첫번째 뷰로 설정
         if (navigationView != null && UIManager.Instance.viewStack.Count == 0)
@@ -29,5 +31,13 @@ public class PanelList : ViewController {
 
     void OnScrollChanged(Vector2 scrollPos){
         cam.transform.position = Vector2.Lerp(minPos, maxPos, scrollPos.y);
+    }
+
+    void AddShoeItemsIntoRow(){
+        UIManager.Instance.itemList.ForEach((item) =>
+        {
+            if(item.isNew) newRow.AddNewShoe(item);
+            if(item.isBest) bestRow.AddNewShoe(item);
+        });
     }
 }

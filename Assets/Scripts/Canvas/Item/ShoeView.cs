773b8f10876112a05a358035147e13a35f13e76c
Wonyeong Choi <ciwnyg0815@gmail.com>
2018-11-06 12:45:33 +0900

    add shoeView & topVIew

---
+++ Assets/Scripts/Canvas/Item/ShoeView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShoeView : MonoBehaviour {
+
+    public Button backBut, arExpBut, arViewBut;
+
+	// Use this for initialization
+	void Start () {
+        backBut.onClick.AddListener(BackButtonClick);
+        arExpBut.onClick.AddListener(ARExpButtonClick);
+        arViewBut.onClick.AddListener(ARViewButtonClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void BackButtonClick(){
+        UIManager.Instance.viewStack.Pop();
+    }
+
+    void ARExpButtonClick(){
+        SceneChanger.ChangeToAttachShoes();
+    }
+
+    void ARViewButtonClick(){
+        SceneChanger.ChangeToWatchingShoes();
+    }
+}
---
+++ Assets/TopView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopView : MonoBehaviour {
+
+    public Button shopBut, arExpBut, arViewBut;
+
+    // Use this for initialization
+    void Start () {
+        shopBut.onClick.AddListener(ShopButtonClick);
+        arExpBut.onClick.AddListener(ARExpButtonClick);
+        arViewBut.onClick.AddListener(ARViewButtonClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ShopButtonClick()
+    {
+        // ToDo : shop panel link activate
+        //UIManager.Instance.shopPanel.url = link;
+        UIManager.Instance.navigationView.Push(UIManager.Instance.shopPanel); 
+    }
+
+    void ARExpButtonClick()
+    {
+        SceneChanger.ChangeToAttachShoes();
+    }
+
+    void ARViewButtonClick()
+    {
+        SceneChanger.ChangeToWatchingShoes();
+    }
+}
